592d82edf5f9712b4ba26328074e4058b8665e4d
sagarpujar <sagarpujar420@gmail.com>
2021-04-09 17:53:33 +0530

    Feat : Input and Outpur Processing is done

--- app.js
+++ app.js
@@ -7,10 +7,11 @@
 
 var btntranslate = document.querySelector("#clickme");
 var userInput = document.querySelector("#userinput"); // get Element Id which user will click
+
+var iserOutput = document.querySelector("#output");
+
 btntranslate.addEventListener("click", function clickEventHandler() {
-  // function to execute when user clicked4
-  console.log("Clicked");
-  console.log("User input is : ", userInput.value);
+  iserOutput.innerText = userInput.value;
 });
 
 // step 3
