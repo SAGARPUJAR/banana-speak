79207a2e40c36b002b183e8a7ec4fa7aa813cd2c
sagarpujar <sagarpujar420@gmail.com>
2021-04-09 18:16:29 +0530

    Latest updates

--- app.js
+++ app.js
@@ -9,9 +9,17 @@ var btntranslate = document.querySelector("#clickme");
 var userInput = document.querySelector("#userinput"); // get Element Id which user will click
 
 var iserOutput = document.querySelector("#output");
+var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+function getTranslate(text) {
+  return serverurl + "/" + "text=" + text;
+}
 
 btntranslate.addEventListener("click", function clickEventHandler() {
-  iserOutput.innerText = userInput.value;
+  //   iserOutput.innerText = userInput.value;
+  var txtInput = userInput.value;
+  fetch(getTranslate(text))
+    .then((Response) => Response.json())
+    .then((json) => console.log(json));
 });
 
 // step 3
