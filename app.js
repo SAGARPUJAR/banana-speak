e97fddc52e7c247aca731d1056cd1ea095f67f13
sagarpujar <sagarpujar420@gmail.com>
2021-04-09 17:30:53 +0530

    Update : Taking input and Console

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var username = prompt("your name ? ");
+// step 1
 
+// var username = prompt("your name ? ");
+// alert("Your name is "+username)
 
-alert("Your name is "+username)
+//step 2
+
+var btntranslate = document.querySelector("#clickme");
+var userInput = document.querySelector("#userinput"); // get Element Id which user will click
+btntranslate.addEventListener("click", function clickEventHandler() {
+  // function to execute when user clicked4
+  console.log("Clicked");
+  console.log("User input is : ", userInput.value);
+});
+
+// step 3
